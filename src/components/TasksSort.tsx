763a5e18713db83a8f66252911dbe5e93e5c0557
maciej-galuszka <maciej.galuszka01@gmail.com>
2025-12-26 22:51:30 +0100

    feat: add TasksSort component

---
+++ src/components/TasksSort.tsx
@@ -0,0 +1,44 @@
+import { Button } from './Button';
+
+type SortBy = 'input' | 'finished' | 'priority' | 'date';
+
+type TasksSortProps = {
+  onClearTasks: () => void;
+  sortBy: SortBy;
+  onSort: (sortBy: SortBy) => void;
+};
+
+export function TasksSort({ onClearTasks, sortBy, onSort }: TasksSortProps) {
+  function handleSortChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    const value = e.currentTarget.value;
+
+    if (
+      value === 'input' ||
+      value === 'finished' ||
+      value === 'priority' ||
+      value === 'date'
+    ) {
+      onSort(value);
+    }
+  }
+
+  return (
+    <>
+      <div className="sort-container">
+        <select
+          className="input-details input-details--sort"
+          value={sortBy}
+          onChange={handleSortChange}
+        >
+          <option value={'input'}>SORT BY INPUT</option>
+          <option value={'finished'}>SORT BY FINISHED</option>
+          <option value={'priority'}>SORT BY PRIORITY</option>
+          <option value={'date'}>SORT BY DATE</option>
+        </select>
+        <Button className="btn--clear" onClick={onClearTasks} type="button">
+          CLEAR TASKS
+        </Button>
+      </div>
+    </>
+  );
+}
